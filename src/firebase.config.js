bb44fd5f6184f8bf5bb5c96a4e50789069a3d259
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-04-11 07:55:52 +0600

    FIREBASE AUTH

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAvNensautgiDAQ253pZVEIVG6WU71Xnrk",
+  authDomain: "ema-jhon-simple60.firebaseapp.com",
+  projectId: "ema-jhon-simple60",
+  storageBucket: "ema-jhon-simple60.appspot.com",
+  messagingSenderId: "259456050617",
+  appId: "1:259456050617:web:009ad473d139e4c2720268"
+};
+export default firebaseConfig;
